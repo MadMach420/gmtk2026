21ffb66f38ab9b58f4adba9a1ed9d439227e3184
eniu <eniu@eniumail.com>
2026-07-23 19:51:09 +0200

    kraftowe assety, usuniecie skoku i fix viewport height

--- src/gameplay/player.gd
+++ src/gameplay/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 150.0
 const PUSH_FORCE = 50
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -11,13 +12,15 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		sprite_2d.flip_h = false
+	elif direction < 0:
+		sprite_2d.flip_h = true
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
