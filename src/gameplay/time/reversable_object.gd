f98ec0a0594c6c7df120338a219f7eb8b543fc18
Krzysztof Mach <mach.krzsztf@gmail.com>
2026-07-23 13:45:57 +0200

    working timer

--- src/gameplay/time/reversable_object.gd
+++ src/gameplay/time/reversable_object.gd
@@ -1,9 +1,13 @@
 extends Node2D
-class_name ReversibleObject
+class_name ReversableObject
+
 
 @onready var time_system: TimeSystem = Systems.get_node("%TimeSystem")
 
-var timeline = []
+# Example snapshot will look like: { "time": 45.2, "data": { "pos": Vector2(...) } }
+var timeline: Array[Dictionary] = []
+var is_rewinding: bool = false
+var current_tween: Tween
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,6 +17,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var time = time_system.get_current_time_left()
+	var time := time_system.get_current_time_left()
+	var time_int_part = int(time)
 	if time > 0:
 		pass
--- src/gameplay/time/time_system.gd
+++ src/gameplay/time/time_system.gd
@@ -1,9 +1,13 @@
 extends Node
 class_name TimeSystem
 
+signal loop_ended
+signal loop_started
+
 @export var loop_length_s = 60
 
 var loop_timer = Timer.new()
+var is_rewinding = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,17 +17,19 @@ func _ready() -> void:
 	loop_timer.timeout.connect(_on_loop_timer_timeout)
 
 func start_time() -> void:
+	is_rewinding = false
+	loop_started.emit()
 	loop_timer.start(loop_length_s)
 	
 func get_current_time_left() -> float:
 	if not loop_timer.is_stopped():
 		return loop_timer.time_left
-	else:
-		return 0
+	return 0.0
 
 func _on_loop_timer_timeout():
-	# Rewind time
-	print("Loop timer ended")
+	is_rewinding = true
+	print("Loop timer ended - Rewinding time")
+	loop_ended.emit()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
