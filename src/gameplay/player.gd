0c309058d3cafbe4bf0cf08012f2aff19329f24b
eniu <eniu@eniumail.com>
2026-07-24 15:38:15 +0200

    BaseLevel added

--- src/gameplay/player.gd
+++ src/gameplay/player.gd
@@ -21,25 +21,24 @@ func _physics_process(delta: float) -> void:
 		if Input.is_action_just_pressed("interact"):
 			has_started_timer = true
 			time_system.start_timer()
-		return
 
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	
-	if direction > 0:
-		sprite_2d.flip_h = false
-	elif direction < 0:
-		sprite_2d.flip_h = true
-	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if has_started_timer:
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("move_left", "move_right")
+		
+		if direction > 0:
+			sprite_2d.flip_h = false
+		elif direction < 0:
+			sprite_2d.flip_h = true
+		
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
 	resolve_collisions()
