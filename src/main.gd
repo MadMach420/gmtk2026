b9d1e21f2da8edbfaa1a6a53f1c16a88924b7867
eniu <eniu@eniumail.com>
2026-07-24 18:36:04 +0200

    level manager

--- src/main.gd
+++ src/main.gd
@@ -5,4 +5,5 @@ var debug_scene = "uid://5buc5jqkuv8y"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	SceneLoader.load_scene(debug_scene)
+	SceneManager._ready()
+	#SceneLoader.load_finished.connect()
