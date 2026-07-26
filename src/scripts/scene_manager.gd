8f6512d59ab677963aec71995089fa26ea280257
MadMach420 <mach.krzysztof@proton.me>
2026-07-26 18:36:51 +0200

    fix

--- src/scripts/scene_manager.gd
+++ src/scripts/scene_manager.gd
@@ -3,7 +3,6 @@ extends Node
 const MAIN_MENU_PATH: NodePath = "res://src/ui/main_menu/MainMenu.tscn"
 const BEGGINING_CUTSCENE_PATH = "" # nalezy to wypelnic
 const LEVEL_PATHS_IN_ORDER: Array[NodePath] = [
-	"res://src/cutscenes/EndingCutscene.tscn",
 	"res://src/levels/tutorial_level/TutorialLevel.tscn",
 	"res://src/levels/krzychu_level/JanekLevel.tscn",
 	"res://src/levels/szymon_level/SzymonLevel.tscn",
