2a0020ae76406dfecb0b640b113516cdd6f3ed04
Nautirius <marcinknapczyk02@gmail.com>
2026-07-26 18:11:20 +0200

    Added new level

--- src/levels/marcin_levels/marcin_level2.gd
+++ src/levels/marcin_levels/marcin_level2.gd
@@ -4,7 +4,11 @@ extends Level
 @onready var reversable_button1: ReversableButton = $Entities/ReversableButton1
 @onready var reversable_button2: ReversableButton = $Entities/ReversableButton2
 @onready var reversable_button3: ReversableButton = $Entities/ReversableButton3
+@onready var reversable_lever: ReversableLever = $Entities/ReversableLever
+@onready var reversable_lever_2: ReversableLever = $Entities/ReversableLever2
 
+@onready var platform: AnimatableBody2D = $Entities/Platform
+@onready var platform_2: AnimatableBody2D = $Entities/Platform2
 @onready var gate1: StaticBody2D = $Entities/Gate1
 @onready var gate2: StaticBody2D = $Entities/Gate2
 @onready var gate3: StaticBody2D = $Entities/Gate3
@@ -15,6 +19,11 @@ func _ready() -> void:
 	reversable_button1.toggled.connect(func(is_pressed: bool): gate1.open() if is_pressed else gate1.close())
 	reversable_button2.toggled.connect(func(is_pressed: bool): gate2.open() if is_pressed else gate2.close())
 	reversable_button3.toggled.connect(func(is_pressed: bool): gate3.open() if is_pressed else gate3.close())
+
+	
+	reversable_lever.toggled.connect(func(is_pressed: bool): platform.extend_platform() if is_pressed else platform.retract_platform())
+	reversable_lever_2.toggled.connect(func(is_pressed: bool): platform_2.extend_platform() if is_pressed else platform_2.retract_platform())
+
 
 func _process(delta: float) -> void:
 	super._process(delta)
